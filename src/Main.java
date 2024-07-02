0662c249364e4f666acfa7442e29cd9c07280048
tjfwltn <tjfwltn1234@gmail.com>
2024-07-02 15:00:15 +0900

    test #2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("테스트");
+        System.out.println("메인 커밋 테스트");
     }
 }
